0fd7c58196c0f2836560adae429db965369ba54c
Bahadır AKKÖY <bahadir.akkoy@gmail.com>
2022-06-03 14:34:07 +0300

    herşey tamam bakalım ne dicek.

---
+++ lib/haberler.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webfeed/domain/rss_feed.dart';
+
+class Haberler extends StatefulWidget {
+  const Haberler({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Haberler> createState() => _HaberlerState();
+}
+
+class _HaberlerState extends State<Haberler> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<RssFeed> haberleriIndir() async {
+    var response = await http.get(
+        Uri.parse('https://www.mynet.com/yerel-haberler/rss/kategori/ana'));
+    return RssFeed.parse(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<RssFeed>(
+      future: haberleriIndir(), // async work
+      builder: (BuildContext context, AsyncSnapshot<RssFeed> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          default:
+            if (snapshot.hasError) {
+              return Center(child: Text('Hata: ${snapshot.error}'));
+            } else {
+              return ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: snapshot.data!.items?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  var title = snapshot.data!.items?[0].title;
+                  var description = snapshot.data!.items?[0].description;
+                  var link = snapshot.data!.items?[0].link;
+
+                  return Card(
+                    elevation: 4,
+                    child: ListTile(
+                      onTap: () async {
+                        await launchUrl(Uri.parse(link!));
+                      },
+                      title: Text(
+                        title ?? 'Yüklenemdi',
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                        maxLines: 1,
+                      ),
+                      leading: const CircleAvatar(
+                        radius: 25.0,
+                        backgroundImage: NetworkImage(
+                            'https://imgrosetta.mynet.com/file/1585522/300x300.jpg'),
+                        backgroundColor: Colors.transparent,
+                      ),
+                      subtitle: Container(
+                        padding: const EdgeInsets.all(4),
+                        child: Text(
+                          description ?? 'Yüklenemdi',
+                          style: const TextStyle(fontSize: 15),
+                          maxLines: 2,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+        }
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:haberuygulamasi/haberler.dart';
+import 'package:haberuygulamasi/hakkimizda.dart';
+import 'package:haberuygulamasi/iletisim.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Haber Uygulaması',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.light,
+        primaryColor: Colors.orange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _aktifSayfa = 0; //Anasayfa
+
+  //menü butonlara basıldıkça içeri yüklenecek sayfafar
+  static const List<Widget> _sayfalar = <Widget>[
+    Haberler(),
+    Hakkimizda(),
+    Iletisim()
+  ];
+
+  //menü butonlara basıldıkça değişecek başlıklar
+  static const List<String> _sayfaBaslgi = <String>[
+    "Anasayfa",
+    "Hakkımızda",
+    "İletişim"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_sayfaBaslgi.elementAt(_aktifSayfa)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.newspaper),
+            label: 'Haberler',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Hakkımızda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_page),
+            label: 'İletişim',
+          ),
+        ],
+        currentIndex: _aktifSayfa, //New
+        onTap: (int index) {
+          setState(() {
+            _aktifSayfa = index;
+          });
+        },
+      ),
+      body: Center(
+        child: _sayfalar.elementAt(_aktifSayfa),
+      ),
+    );
+  }
+}
